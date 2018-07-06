8cc69db2c49661a7573d77202f93cd03fec203de
Eric Strong <ericstrong89@gmail.com>
2018-07-06 07:57:43 +0100

    websocket

---
+++ src/app/chat.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs/Rx';
+import { WebsocketService } from './websocket.service';
+
+const CHAT_URL = 'ws://echo.websocket.org/'; //change this to our websocket
+
+export interface Message {
+	message: string
+}
+
+@Injectable()
+export class ChatService {
+	public messages: Subject<Message>;
+
+	constructor(wsService: WebsocketService) {
+		this.messages = <Subject<Message>>wsService
+			.connect(CHAT_URL)
+			.map((response: MessageEvent): Message => {
+				let data = JSON.parse(response.data);
+				return {
+
+					message: data.message
+				}
+			});
+	}
+}
